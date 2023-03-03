040f448fbb30a9a6e62ac735f4365d2e3b6c99a2
David <d.elbaze@gmail.com>
2023-03-03 12:35:12 +0100

    mock apollo server

---
+++ src/tests/mock.spec.ts
@@ -0,0 +1,49 @@
+import { ApolloServer, gql } from "apollo-server";
+import { faker } from "@faker-js/faker";
+
+const typeDefs = gql`
+  type Query {
+    listUsers: [User]
+  }
+  type User {
+    nom: String
+    prenom: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    listUsers: () => {},
+  },
+};
+
+const mocks = {
+  User: () => ({ nom: faker.name.firstName(), prenom: faker.name.lastName() }),
+};
+const GET_USER = gql`
+  query ListUsers {
+    listUsers {
+      nom
+      prenom
+    }
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  mocks,
+});
+
+beforeAll(async () => {
+  await server.listen({ port: 4006 });
+  console.log(`🚀 Server ready`);
+});
+
+test("Test", async () => {
+  const result = await server.executeOperation({
+    query: GET_USER,
+    // variables: { id: 1 }
+  });
+  expect(result.data?.listUsers).not.toHaveLength(0);
+});
